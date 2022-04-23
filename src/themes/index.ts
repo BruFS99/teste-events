6afa3cdf517b4016f8cdae8a7fe868c7818a1487
chrigor <chrigor.edu3@gmail.com>
2022-04-22 22:08:22 -0300

    feat: :sparkles: Adicionando import de fonte e adicionando tokens que ser]ao necessários

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -2,10 +2,34 @@ const theme = {
   colors: {
     red: '#FE1211',
     gray: '#5F5F5F',
+    grayMedium: '#8c8c8c',
     pink: '#FBAFAF',
     pinkLight: '#FDECEC',
-    grayLight: '#E5E5E5',
     white: '#FFF',
+  },
+  spacing: {
+    quark: '4px',
+    nano: '8px',
+  },
+  font: {
+    family: {
+      primary: '"Work Sans", sans-serif',
+    },
+    size: {
+      xsmall: '10px',
+      small: '14px',
+      medium: '16px',
+      large: '24px',
+      xlarge: '36px',
+      xxlarge: '48px',
+    },
+    weight: {
+      black: 900,
+      bold: 700,
+      semiBold: 500,
+      regular: 400,
+      light: 300,
+    },
   },
 }
 
--- styled.d.ts
+++ styled.d.ts
@@ -1,15 +1,5 @@
 import 'styled-components'
 
-interface IColors {
-  red: string
-  gray: string
-  pink: string
-  pinkLight: string
-  grayLight: string
-}
-
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: IColors
-  }
+  export interface DefaultTheme {}
 }
