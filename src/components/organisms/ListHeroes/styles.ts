dbad22aecc95c34068bb2371c94c7e10975827aa
chrigor <chrigor.edu3@gmail.com>
2022-04-23 15:12:28 -0300

    feat: :sparkles: Usando componente de listagem de herois

--- src/components/organisms/ListHeroes/styles.ts
+++ src/components/organisms/ListHeroes/styles.ts
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 import Text from 'components/atoms/Text'
 
 export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.medium};
   width: 100%;
 `
 
@@ -21,4 +24,25 @@ export const WrapperActions = styled.div`
 export const TextInfo = styled(Text)`
   color: ${({ theme }) => theme.colors.grayLight};
   font-size: ${({ theme }) => theme.font.size.medium};
+`
+export const WrapperList = styled.ul`
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: ${({ theme }) => theme.spacing.medium};
+
+  width: 100%;
+  height: 100%;
+
+  @media (max-width: 1280px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 968px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 500px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
