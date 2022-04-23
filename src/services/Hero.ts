872c3d00895a12e34caf08838fb834a8793b31c9
chrigor <chrigor.edu3@gmail.com>
2022-04-23 15:30:28 -0300

    refactor: :recycle: Usando interface junto com service para facilitar

--- src/services/Hero.ts
+++ src/services/Hero.ts
@@ -1,9 +1,27 @@
-import api from 'api';
+import api from 'api'
+
+export interface IHero {
+  id: number
+  name: string
+  thumbnail: {
+    path: string
+    extension: string
+  }
+}
+
+export interface IListHeroResponse {
+  data: {
+    count: string
+    limit: string
+    offset: string
+    results: IHero[]
+  }
+}
 
 function getHeroes() {
-  return api.get('/characters');
+  return api.get('/characters')
 }
 
 export default {
   getHeroes,
-};
+}
