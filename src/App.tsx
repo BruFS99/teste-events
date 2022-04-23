7644a9309358c47afa623d746acaaa2e8308dd9e
chrigor <chrigor.edu3@gmail.com>
2022-04-23 16:32:33 -0300

    feat: :sparkles: Guardando estado dos favoritos no redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { ThemeProvider } from 'styled-components'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import Routes from 'Routes'
 
 import theme from 'themes'
 import GlobalStyles from 'styles/Global'
 
-import store from './store'
+import { store, persistor } from './store'
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <GlobalStyles />
-          <Routes />
-        </div>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <div className="App">
+            <GlobalStyles />
+            <Routes />
+          </div>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   )
 }
