df80c9af8e97ee65ce7578a06c3f034acbf1eb51
chrigor <chrigor.edu3@gmail.com>
2022-04-23 15:30:08 -0300

    refactor: :recycle: Trocando nome de componente e utilizando novo componente criado

---
+++ src/components/organisms/SectionHeroes/index.tsx
@@ -0,0 +1,26 @@
+import OrderByName from 'components/molecules/OrderByName'
+import OnlyFavorites from 'components/molecules/OnlyFavorites'
+import ListHeroes from 'components/molecules/ListHeroes'
+
+import useHeroes from 'hooks/useHeroes'
+
+import { Container, Header, WrapperActions, TextInfo } from './styles'
+
+function SectionHeroes() {
+  const { heroes, loading, error } = useHeroes()
+
+  return (
+    <Container>
+      <Header>
+        <TextInfo>Encontrados {heroes.length} heróis</TextInfo>
+        <WrapperActions>
+          <OrderByName />
+          <OnlyFavorites />
+        </WrapperActions>
+      </Header>
+      <ListHeroes heroes={heroes} loading={loading} error={error} />
+    </Container>
+  )
+}
+
+export default SectionHeroes
---
+++ src/components/organisms/SectionHeroes/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+import Text from 'components/atoms/Text'
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.medium};
+  width: 100%;
+`
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+
+  justify-content: space-between;
+`
+
+export const WrapperActions = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing.small};
+`
+
+export const TextInfo = styled(Text)`
+  color: ${({ theme }) => theme.colors.grayLight};
+  font-size: ${({ theme }) => theme.font.size.medium};
+`
