435f9909ddd110c01009478de6adc98575499ffe
chrigor <chrigor.edu3@gmail.com>
2022-04-23 15:10:31 -0300

    refactor: :recycle: Refatorando componente para usar toggle

--- src/components/molecules/Action/index.tsx
+++ src/components/molecules/Action/index.tsx
@@ -1,17 +1,24 @@
 import Icon from 'components/atoms/Icon'
+import Toggle from 'components/molecules/Toggle'
 
 import { Container, Label } from './styles'
 
 interface IActionsProps {
+  initialValue: boolean
   text: string
   source: string
-  onClick: () => void
+  onChange: Function
 }
 
-function Action({ text, source, onClick }: IActionsProps) {
+function Action({ initialValue, text, source, onChange }: IActionsProps) {
   return (
-    <Container onClick={onClick}>
-      <Icon src={source} />
+    <Container>
+      <Toggle
+        initialValue={initialValue}
+        activeComponent={<Icon src={source} />}
+        disabledComponent={<Icon src={source} />}
+        onChange={onChange}
+      />
       <Label>{text}</Label>
     </Container>
   )
