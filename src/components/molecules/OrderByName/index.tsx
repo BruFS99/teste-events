9ff3ad181906add8a50ccdc2cfaa7888989edaf4
chrigor <chrigor.edu3@gmail.com>
2022-04-23 11:32:48 -0300

    feat: :sparkles: Criando componente de actiond e order by name

---
+++ src/components/molecules/OrderByName/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+import Icon from 'components/atoms/Icon'
+import Action from 'components/molecules/Action'
+
+import ToggleOff from 'assets/toggle_off.svg'
+import ToggleOn from 'assets/toggle_on.svg'
+import HeroIcon from 'assets/ic_heroi.svg'
+
+import { Container } from './styles'
+
+function OrderByName() {
+  const [order, setOrder] = useState(false)
+
+  const handleClick = () => {
+    setOrder((prev) => !prev)
+  }
+
+  const source = order ? ToggleOn : ToggleOff
+
+  return (
+    <Container>
+      <Action
+        text="Ordenar por nome - A/Z"
+        source={HeroIcon}
+        onClick={handleClick}
+      />
+      <Icon src={source} size="48px" onClick={handleClick} />
+    </Container>
+  )
+}
+
+export default OrderByName
