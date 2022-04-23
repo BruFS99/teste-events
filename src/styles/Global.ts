5f5ac8596e61a1e91b0e5cca1e791e7f20b90e71
chrigor <chrigor.edu3@gmail.com>
2022-04-22 22:09:25 -0300

    refactor: :recycle: Usando fonte de forma global e adicionando background color no body

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -1,6 +1,47 @@
 import { createGlobalStyle } from 'styled-components'
 
-export default createGlobalStyle`
+export interface IColors {
+  red: string
+  gray: string
+  pink: string
+  pinkLight: string
+  grayMedium: string
+  white: string
+}
+
+export interface ISpacing {
+  quark: string
+  nano: string
+}
+
+export interface IFont {
+  family: {
+    primary: string
+  }
+  size: {
+    xsmall: string
+    small: string
+    medium: string
+    large: string
+    xlarge: string
+    xxlarge: string
+  }
+  weight: {
+    black: string
+    bold: string
+    semiBold: string
+    regular: string
+    light: string
+  }
+}
+
+export interface ITheme {
+  colors: IColors
+  spacing: ISpacing
+  font: IFont
+}
+
+export default createGlobalStyle<{ theme: ITheme }>`
   * {
     margin: 0;
     padding:0;
@@ -15,6 +56,10 @@ export default createGlobalStyle`
   *, button, input {
     border:0;
     background:none;
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: ${({ theme }) => theme.font.family.primary};
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.colors.white};
   }
 `
