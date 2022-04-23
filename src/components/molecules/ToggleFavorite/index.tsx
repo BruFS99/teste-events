470c109530258217f83e2e411a87c6c547ed495c
chrigor <chrigor.edu3@gmail.com>
2022-04-23 15:11:39 -0300

    feat: :sparkles: Criando componente de toggle favorite para reusa-lo

---
+++ src/components/molecules/ToggleFavorite/index.tsx
@@ -0,0 +1,23 @@
+import Toggle from 'components/molecules/Toggle'
+
+import EmptyHeartIcon from 'assets/favorito_01.svg'
+import FullHeartIcon from 'assets/favorito_02.svg'
+import Icon from 'components/atoms/Icon'
+
+interface IToggleFavoriteProps {
+  initialValue: boolean
+  onChange: Function
+}
+
+function ToggleFavorite({ initialValue, onChange }: IToggleFavoriteProps) {
+  return (
+    <Toggle
+      initialValue={initialValue}
+      activeComponent={<Icon src={EmptyHeartIcon} size="14px" />}
+      disabledComponent={<Icon src={FullHeartIcon} size="14px" />}
+      onChange={onChange}
+    />
+  )
+}
+
+export default ToggleFavorite
