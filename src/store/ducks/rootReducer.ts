7c4a221e6222113b2b5ff7226efb9d265fbf6d1e
chrigor <chrigor.edu3@gmail.com>
2022-04-23 16:31:20 -0300

    feat: :sparkles: Adicionando estado global de herois favoritos no root reducer

--- src/store/ducks/rootReducer.ts
+++ src/store/ducks/rootReducer.ts
@@ -1,13 +1,16 @@
 import { combineReducers } from 'redux'
 
 import OrderByListHeroes, { IOrderByListHeroes } from './OrderByListHeroes'
+import FavoriteHeroes, { IFavoriteHeroes } from './FavoriteHeroes'
 
 export interface IRootState {
   OrderByListHeroes: IOrderByListHeroes
+  FavoriteHeroes: IFavoriteHeroes
 }
 
 const appReducer = combineReducers({
   OrderByListHeroes,
+  FavoriteHeroes,
 })
 
 export default appReducer
