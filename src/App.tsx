f16d9175b9f5b112b278b6e2e7bbf3365b0e18b0
chrigor <chrigor.edu3@gmail.com>
2022-04-23 16:09:19 -0300

    feat: :sparkles: Adicionando provider da store para toda aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux'
+
+import Routes from 'Routes'
+
 import theme from 'themes'
 import GlobalStyles from 'styles/Global'
-import Routes from 'Routes'
+
+import store from './store'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <GlobalStyles />
-        <Routes />
-      </div>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <GlobalStyles />
+          <Routes />
+        </div>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
