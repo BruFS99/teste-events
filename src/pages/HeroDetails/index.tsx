ed0c67a23df79ca20090dcaf8d1bbb6358912ac0
chrigor <chrigor.edu3@gmail.com>
2022-04-24 19:04:11 -0300

    feat: :sparkles: Utilizando componentes criados na pagina em questão

--- src/pages/HeroDetails/index.tsx
+++ src/pages/HeroDetails/index.tsx
@@ -1,13 +1,39 @@
 import { useParams } from 'react-router-dom'
 
 import HeroDetailsLayout from 'layout/HeroDetails'
+import SectionHero from 'components/organisms/SectionHero'
+import ListComics from 'components/organisms/ListComics'
+
+import useHero from 'hooks/useHero'
+import useHeroComics from 'hooks/useHeroComics'
+
+import { IComicDate } from 'services/Hero'
+import { getDateFormatedBR } from 'utils/Date'
+
+import { Container } from './styles'
 
 function HeroDetails() {
-  const params = useParams()
+  const { id } = useParams()
+
+  const { hero, loading } = useHero({ id })
+  const { comics } = useHeroComics({ id })
+
+  const getLastDateComic = (date: IComicDate) => date.type === 'onsaleDate'
+
+  const lastComic = comics?.length ? comics[0] : null
+  const dateLastComic = lastComic?.dates?.find(getLastDateComic)?.date || ''
+  const dateLastComicFormatted = getDateFormatedBR(dateLastComic)
+
+  if (loading) {
+    return <h3>loading ... </h3>
+  }
 
   return (
     <HeroDetailsLayout>
-      <div>HeroDetails: {params.id}</div>
+      <Container>
+        <SectionHero hero={hero} dateLastComic={dateLastComicFormatted} />
+        <ListComics comics={comics} />
+      </Container>
     </HeroDetailsLayout>
   )
 }
