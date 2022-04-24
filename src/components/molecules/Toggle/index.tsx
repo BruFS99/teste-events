decf1660f937afcde4b425ca30d73ecb81ed691a
chrigor <chrigor.edu3@gmail.com>
2022-04-24 13:33:44 -0300

    refactor: :recycle: Deixando estado por fora do componente

--- src/components/molecules/Toggle/index.tsx
+++ src/components/molecules/Toggle/index.tsx
@@ -1,35 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { Container } from './styles'
 
 interface IToggle {
   activeComponent: React.ReactNode
   disabledComponent: React.ReactNode
-  initialValue: boolean
+  value: boolean
   onChange: Function
 }
 
 function Toggle({
-  initialValue,
+  value,
   activeComponent,
   disabledComponent,
   onChange,
 }: IToggle) {
-  const [active, setActive] = useState(false)
-
   const handleClick = () => {
-    const newValue = !active
-    setActive(newValue)
+    const newValue = !value
     onChange(newValue)
   }
 
-  useEffect(() => {
-    setActive(initialValue)
-  }, [initialValue])
-
   return (
     <Container onClick={handleClick}>
-      {active ? activeComponent : disabledComponent}
+      {value ? activeComponent : disabledComponent}
     </Container>
   )
 }
