d48b7f86ba4970cdab120d32210c7be3ba43af93
chrigor <chrigor.edu3@gmail.com>
2022-04-23 22:42:16 -0300

    refactor: :recycle: Deixando componente puro

--- src/components/molecules/OnlyFavorites/index.tsx
+++ src/components/molecules/OnlyFavorites/index.tsx
@@ -1,28 +1,25 @@
-import { useState } from 'react'
-
 import Action from 'components/molecules/Action'
 
-import EmptyHeartIcon from 'assets/favorito_01.svg'
-import FullHeartIcon from 'assets/favorito_02.svg'
+import EmptyHeartIcon from 'assets/favorito_02.svg'
+import FullHeartIcon from 'assets/favorito_01.svg'
 
 import { Container } from './styles'
 
-function OnlyFavorites() {
-  const [favorites, setFavorites] = useState(false)
+interface IOnlyFavoritesProps {
+  value: boolean
+  onChange: Function
+}
 
-  const handleClick = () => {
-    setFavorites((prev) => !prev)
-  }
-
-  const source = favorites ? FullHeartIcon : EmptyHeartIcon
+function OnlyFavorites({ value, onChange }: IOnlyFavoritesProps) {
+  const source = value ? FullHeartIcon : EmptyHeartIcon
 
   return (
     <Container>
       <Action
-        initialValue={favorites}
+        initialValue={value}
         text="Somente favoritos"
         source={source}
-        onChange={handleClick}
+        onChange={onChange}
       />
     </Container>
   )
