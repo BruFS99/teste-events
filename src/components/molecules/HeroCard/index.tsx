5bc02fd4b9cf188a8cb169c7df05e2e333bf94af
chrigor <chrigor.edu3@gmail.com>
2022-04-24 13:50:32 -0300

    feat: :sparkles: Adicionando funcao de onclick

--- src/components/molecules/HeroCard/index.tsx
+++ src/components/molecules/HeroCard/index.tsx
@@ -2,15 +2,24 @@ import ToggleFavorite from 'components/molecules/ToggleFavorite'
 import { Bar, Container, HeroInfo, Image, Name, WrapperImage } from './styles'
 
 interface ICardProps {
+  id: number
   src: string
   name: string
   isFavorite: boolean
   onFavoriteChange: Function
+  onClick: Function
 }
 
-function HeroCard({ src, name, isFavorite, onFavoriteChange }: ICardProps) {
+function HeroCard({
+  id,
+  src,
+  name,
+  isFavorite,
+  onFavoriteChange,
+  onClick,
+}: ICardProps) {
   return (
-    <Container>
+    <Container onClick={() => onClick(id)}>
       <WrapperImage>
         <Image src={src} alt={name} />
         <Bar />
