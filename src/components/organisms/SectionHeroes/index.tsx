9462a4d196cabba8d5c69ce63cea262b21f49722
chrigor <chrigor.edu3@gmail.com>
2022-04-24 11:35:39 -0300

    feat: :sparkles: Adicionando logica de herois favoritos (Adicionar, remover, listar)

--- src/components/organisms/SectionHeroes/index.tsx
+++ src/components/organisms/SectionHeroes/index.tsx
@@ -1,19 +1,29 @@
+import { useSelector } from 'react-redux'
 import { useSearchParams } from 'react-router-dom'
+
 import OrderByName from 'components/molecules/OrderByName'
 import OnlyFavorites from 'components/molecules/OnlyFavorites'
 import ListHeroes from 'components/molecules/ListHeroes'
 
 import useHeroes from 'hooks/useHeroes'
-import useOrderByName from 'pages/Home/hooks/useOrderByName'
+import useFavoriteHeroes from 'store/ducks/FavoriteHeroes/useFavoritesHeroes'
+import useOrderByName from 'components/organisms/SectionHeroes/hooks/useOrderByName'
 
-import { useSelector } from 'react-redux'
+import { IHero } from 'services/Hero'
 import { IRootState } from 'store/ducks/rootReducer'
 
 import { Container, Header, WrapperActions, TextInfo } from './styles'
+import useFavoritesVisible from './hooks/useFavoritesVisible'
+import useHeroesVisible from './hooks/useHeroesVisible'
 
 function SectionHeroes() {
   const [searchParams] = useSearchParams()
+
+  const { OrderByListHeroes, FavoriteHeroes } = useSelector(
+    (state: IRootState) => state
+  )
   const { orderBy } = OrderByListHeroes
+  const { favorites: favoriteHeroes } = FavoriteHeroes
 
   const search = searchParams.get('search')
 
@@ -21,21 +31,54 @@ function SectionHeroes() {
     orderBy,
     search,
   })
+
+  const { handleAddFavoriteHero, handleRemoveFavoriteHero } =
+    useFavoriteHeroes()
+
   const { handleToggleOrderByName, isOrderByName } = useOrderByName()
+
+  const {
+    isVisible: favoriteHeroesIsVisible,
+    handleToggle: handleToggleVisibleFavoritHeroes,
+  } = useFavoritesVisible()
+
+  const handleFavoriteChange = (hero: IHero, newValueFavorite: boolean) => {
+    if (newValueFavorite) {
+      handleAddFavoriteHero(hero)
+    } else {
+      handleRemoveFavoriteHero(hero.id)
+    }
+  }
+
+  const { listHeroes } = useHeroesVisible({
+    favoriteHeroes,
+    heroes,
+    favoriteHeroesIsVisible,
+    isOrderByName,
+    search,
+  })
 
   return (
     <Container>
       <Header>
-        <TextInfo>Encontrados {heroes.length} heróis</TextInfo>
+        <TextInfo>Encontrados {listHeroes.length} heróis</TextInfo>
         <WrapperActions>
           <OrderByName
             value={isOrderByName}
             handleClick={handleToggleOrderByName}
           />
-          <OnlyFavorites />
+          <OnlyFavorites
+            value={favoriteHeroesIsVisible}
+            onChange={handleToggleVisibleFavoritHeroes}
+          />
         </WrapperActions>
       </Header>
-      <ListHeroes heroes={heroes} loading={loading} error={error} />
+      <ListHeroes
+        heroes={listHeroes}
+        loading={loading}
+        error={error}
+        onChangeFavorite={handleFavoriteChange}
+      />
     </Container>
   )
 }
