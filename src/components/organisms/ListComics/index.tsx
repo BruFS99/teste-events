13bfcade0f6feacd6598253e886bd5fcf4fccc27
chrigor <chrigor.edu3@gmail.com>
2022-04-24 19:03:27 -0300

    feat: :sparkles: Criando componente de ListComic

---
+++ src/components/organisms/ListComics/index.tsx
@@ -0,0 +1,31 @@
+import SubTitle from 'components/atoms/Subtitle'
+import ComicCard from 'components/molecules/ComicCard'
+import { IComic } from 'services/Hero'
+
+import { Container, WrapperList } from './styles'
+
+interface IListComicsProps {
+  comics: IComic[] | null
+}
+
+function ListComics({ comics }: IListComicsProps) {
+  if (!comics) {
+    return <div />
+  }
+
+  return (
+    <Container>
+      <SubTitle>Últimos lançamentos</SubTitle>
+      <WrapperList>
+        {comics.map((comic) => (
+          <ComicCard
+            {...comic}
+            src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+          />
+        ))}
+      </WrapperList>
+    </Container>
+  )
+}
+
+export default ListComics
---
+++ src/components/organisms/ListComics/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.large};
+  height: 100%;
+`
+
+export const WrapperList = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  gap: ${({ theme }) => theme.spacing.xlarge};
+`
