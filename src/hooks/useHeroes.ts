918e1d1d18cb7642ba68747b7e19192f9191e40e
chrigor <chrigor.edu3@gmail.com>
2022-04-25 19:24:08 -0300

    feat: :sparkles: Adicionando paginação para carregar mais heróis

--- src/hooks/useHeroes.ts
+++ src/hooks/useHeroes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react'
 import { AxiosResponse } from 'axios'
 
@@ -6,33 +7,45 @@ import HeroService, { IHeroResponse, IHero } from 'services/Hero'
 interface IUserHeroes {
   orderBy?: string
   search?: string | null
+  limit?: number | null
 }
 
-function useHeroes({ orderBy = 'name', search = null }: IUserHeroes) {
+function useHeroes({
+  orderBy = 'name',
+  search = null,
+  limit = 20,
+}: IUserHeroes) {
+  const [total, setTotal] = useState(0)
+  const [offset, setOffset] = useState(0)
+  const [loadingMore, setLoadingMore] = useState(false)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
   const [heroes, setHeroes] = useState<IHero[]>([])
 
-  const handleSuccess = (response: AxiosResponse<IHeroResponse>) => {
+  const handleSuccess = (
+    response: AxiosResponse<IHeroResponse>
+  ): AxiosResponse<IHeroResponse> => {
     const { data } = response
-    const { results } = data.data
+    const { results, total: totalHeroes } = data.data
 
     setHeroes(results)
+    setTotal(totalHeroes)
+
+    return response
   }
 
-  const handleError = () => {
-    console.error(error)
-    setError(true)
-  }
+  const handleError = () => setError(true)
 
-  const handleFinally = () => {
-    setLoading(false)
-  }
+  const handleFinally = () => setLoading(false)
+
+  const handleFinallyLoadingMore = () => setLoadingMore(false)
 
   const getParamsFetch = () => {
     const params = {
       nameStartsWith: search,
       orderBy,
+      offset,
+      limit,
     }
 
     if (!params.nameStartsWith) {
@@ -43,25 +56,71 @@ function useHeroes({ orderBy = 'name', search = null }: IUserHeroes) {
     return params
   }
 
-  const fetchData = () => {
-    setLoading(true)
+  const fetchData = (params?: Object) => HeroService.getHeroes(params)
+
+  const handleAddHeroes = (
+    response: AxiosResponse<IHeroResponse>,
+    addInState: boolean
+  ) => {
+    if (!addInState) {
+      return
+    }
+
+    const { data } = response.data
+    const { results } = data
+    const newData = [...heroes, ...results]
+    setHeroes(newData)
+  }
+
+  const getAndSetData = (params?: Object, addInState?: boolean) =>
+    fetchData(params)
+      .then(handleSuccess)
+      .then((response) => handleAddHeroes(response, Boolean(addInState)))
+      .catch(handleError)
+      .finally(handleFinally)
+
+  const loadMore = () => {
+    if (!limit) {
+      return
+    }
+
+    setLoadingMore(true)
 
     const params = getParamsFetch()
 
-    HeroService.getHeroes(params)
-      .then(handleSuccess)
-      .catch(handleError)
-      .finally(handleFinally)
+    const newOffset = offset + limit
+    const newParams = { ...params, offset: newOffset }
+
+    setOffset(newOffset)
+    getAndSetData(newParams, true).finally(handleFinallyLoadingMore)
+  }
+
+  const getNextPage = () => {
+    loadMore()
+  }
+
+  const firstFetch = () => {
+    setLoading(true)
+
+    const params = getParamsFetch()
+    const newParams = { ...params, offset: 0 }
+    setOffset(0)
+
+    getAndSetData(newParams).finally(handleFinally)
   }
 
   useEffect(() => {
-    fetchData()
+    firstFetch()
   }, [orderBy, search])
 
   return {
     loading,
     error,
     heroes,
+    hasMore: total > heroes.length,
+    getNextPage,
+    loadingMore,
+    total,
   }
 }
 
