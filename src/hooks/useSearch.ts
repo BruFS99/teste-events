be80796b6f566b81939d34856e644d16798ac947
chrigor <chrigor.edu3@gmail.com>
2022-04-25 19:23:43 -0300

    fix: :bug: O input de search estava iniciando com null por conta disto

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,7 +1,7 @@
 import { useSearchParams } from 'react-router-dom'
 
 function useSearch() {
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams({})
 
   const handleSubmit = (search: string) => {
     if (!search) {
