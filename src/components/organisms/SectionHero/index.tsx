643874109f374eacc03dc393ce9e829ceaef76f9
chrigor <chrigor.edu3@gmail.com>
2022-04-26 09:53:18 -0300

    refactor: :recycle: Usando componente refatorado

--- src/components/organisms/SectionHero/index.tsx
+++ src/components/organisms/SectionHero/index.tsx
@@ -1,27 +1,9 @@
 import React from 'react'
 
-import LastComic from 'components/molecules/LastComic'
-import HeroShortInfo from 'components/molecules/HeroShortInfo'
-import ToggleFavorite from 'components/molecules/ToggleFavorite'
-import Rating from 'components/molecules/Rating'
-
 import { IHero } from 'services/Hero'
+import HeroInfo from 'components/organisms/HeroInfo'
 
-import IconComic from 'assets/ic_quadrinhos.svg'
-import IconFilm from 'assets/ic_trailer.svg'
-
-import {
-  // BackgroundName,
-  Container,
-  Description,
-  Name,
-  Image,
-  WrapperHeroInfo,
-  WrapperHeroImage,
-  WrapperNameAndFavorite,
-  WrapperRatingHero,
-  WrapperFilmAndComics,
-} from './styles'
+import { Container, Image, WrapperHeroImage } from './styles'
 
 interface ISectionHeroProps {
   hero: IHero | null
@@ -38,39 +20,12 @@ function SectionHero({
 }: ISectionHeroProps) {
   return (
     <Container>
-      {/* <BackgroundName>{String(hero?.name).toUpperCase()}</BackgroundName> */}
-      <WrapperHeroInfo>
-        <WrapperNameAndFavorite>
-          <Name>{hero?.name}</Name>
-          <ToggleFavorite
-            initialValue={isFavorite}
-            onChange={(newValueFavorite: boolean) =>
-              onChangeFavorite(hero, newValueFavorite)
-            }
-            size="30px"
-          />
-        </WrapperNameAndFavorite>
-        <Description>{hero?.description}</Description>
-        <WrapperRatingHero>
-          <WrapperFilmAndComics>
-            <HeroShortInfo
-              title="Quadrinhos"
-              icon={IconComic}
-              number={hero?.comics?.available}
-            />
-
-            <HeroShortInfo
-              title="Filmes"
-              icon={IconFilm}
-              number={hero?.series?.available}
-            />
-          </WrapperFilmAndComics>
-
-          <Rating title="Rating" number={5} />
-
-          <LastComic title="Último quadrinho" value={dateLastComic} />
-        </WrapperRatingHero>
-      </WrapperHeroInfo>
+      <HeroInfo
+        hero={hero}
+        dateLastComic={dateLastComic}
+        onChangeFavorite={onChangeFavorite}
+        isFavorite={isFavorite}
+      />
       <WrapperHeroImage>
         <Image src={`${hero?.thumbnail.path}.${hero?.thumbnail.extension}`} />
       </WrapperHeroImage>
--- src/components/organisms/SectionHero/styles.ts
+++ src/components/organisms/SectionHero/styles.ts
@@ -1,8 +1,5 @@
 import styled from 'styled-components'
 
-import Text from 'components/atoms/Text'
-import SubTitle from 'components/atoms/Subtitle'
-
 import { ITheme } from 'styles/Global'
 
 interface IProps {
@@ -30,16 +27,6 @@ export const Container = styled.section`
     grid-template-columns: 1fr;
   }
 `
-
-export const Name = styled(SubTitle)`
-  font-size: ${({ theme }) => theme.font.size.xlarge};
-  text-transform: uppercase;
-`
-
-export const Description = styled(Text)`
-  line-height: ${({ theme }) => theme.font.lineHeight.medium};
-  font-weight: ${({ theme }) => theme.font.weight.semiBold};
-`
 export const Image = styled.img`
   width: auto;
   height: 400px;
@@ -49,46 +36,8 @@ export const Image = styled.img`
     height: auto;
   }
 `
-export const BackgroundName = styled.div`
-  font-size: 17em;
-  letter-spacing: ${({ theme }) => theme.spacing.medium};
-  color: white;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-
-  transform: translate(-50%, -50%);
-
-  z-index: 0;
-`
-export const WrapperNameAndFavorite = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: ${getGap};
-`
-
-export const WrapperHeroInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${({ theme }) => theme.spacing.large};
-`
 
 export const WrapperHeroImage = styled.div`
   display: flex;
   justify-content: center;
-`
-
-export const WrapperRatingHero = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${({ theme }) => theme.spacing.small};
-`
-export const WrapperFilmAndComics = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  width: 100%;
-  gap: ${({ theme }) => theme.spacing.xsmall};
 `
